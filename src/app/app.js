428140a2dd3ef0566e8b5c7eca302bc88be98d2a
Nishat Hoque <hoquen0397@gmail.com>
2021-04-01 14:07:50 -0400

    fix server issue

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { CLIENT_ORIGIN } = require('../config')
+const { CLIENT_ORIGIN } = require('../../config')
 const peopleRouter = require('../people/people.router')
 const petsRouter = require('../pets/pets.router')
 const catsRouter = require('../cats/cats.router')
@@ -16,3 +16,5 @@ app.use('/api', petsRouter);
 app.use('/api', catsRouter);
 app.use('/api', dogsRouter);
 app.use(errorHandler)
+
+module.exports = app
