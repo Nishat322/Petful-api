16ab5e728a65adc372fdf3b631a7891954c91612
Nishat Hoque <hoquen0397@gmail.com>
2021-03-31 23:17:58 -0400

    Queue.js

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const cors = require('cors')
+const { CLIENT_ORIGIN } = require('../../config')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({origin: CLIENT_ORIGIN}))
 
-app.use('/people', require('../people/people.router'))
-app.use('/pets', require('../pets/pets.router'))
+app.use("/api", require("../people/people.router"));
+app.use("/api", require("../pets/pets.router"));
+app.use("/api", require("../cats/cats.router"));
+app.use("/api", require("../dogs/dogs.router"));
+
+app.use(function errorHandler(error, req, res, next){
+    let response
+    console.error(error)
+    if(NODE_ENV === 'production'){
+        response = {error: { message: 'server error'}}
+    } else {
+        response = { message: error.message, error}
+    }
+    res.status(500).json(response)
+})
 
 module.exports = app
--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,23 +1,63 @@
-class Queue {
-  constructor() {
-    // Set initial data.
-  }
-
-  enqueue(data) {
-    // Add some data to the queue.
-  }
-
-  dequeue() {
-    // Remove some data from the queue.
-  }
-
-  show() {
-    // Return the next item in the queue.
-  }
-
-  all() {
-    // Return all items in the queue.
+class Node {
+  //set initial data
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-module.exports = Queue
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // Add some data to the queue.
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+  }
+
+  // Remove some data from the queue.
+  dequeue() {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
+
+  // Return the next item in the queue.
+  show() {
+    if (!this.first) return null;
+    const temp = this.first;
+    return temp.val;
+  }
+
+  // Return all items in the queue.
+  all() {
+    if (!this.first) return null;
+
+    let tempArr = [];
+    let temp = this.first;
+
+    while (temp) {
+      tempArr.push(temp.val);
+      temp = temp.next;
+    }
+    return tempArr;
+  }
+}
+
+module.exports = Queue;
