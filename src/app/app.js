c988b144e13462c1badd1f472be36f30168c5e21
Nishat Hoque <hoquen0397@gmail.com>
2021-04-01 14:41:56 -0400

    delete unnecessary endpoints

--- src/app/app.js
+++ src/app/app.js
@@ -3,8 +3,6 @@ const cors = require('cors')
 const { CLIENT_ORIGIN } = require('../../config')
 const peopleRouter = require('../people/people.router')
 const petsRouter = require('../pets/pets.router')
-const catsRouter = require('../cats/cats.router')
-const dogsRouter = require('../dogs/dogs.router')
 const errorHandler = require('../errorHandler')
 
 const app = express()
@@ -13,8 +11,6 @@ app.use(cors({origin: CLIENT_ORIGIN}))
 
 app.use('/api', peopleRouter)
 app.use('/api', petsRouter);
-app.use('/api', catsRouter);
-app.use('/api', dogsRouter);
 app.use(errorHandler)
 
 module.exports = app
