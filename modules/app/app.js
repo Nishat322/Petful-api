6497dd119eebe23902e3e85f977e56104123d8cc
Nishat Hoque <hoquen0397@gmail.com>
2021-04-01 13:27:02 -0400

    deployment prep

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-const { CLIENT_ORIGIN } = require('../../config')
+const { CLIENT_ORIGIN } = require('../config')
 const peopleRouter = require('../people/people.router')
 const petsRouter = require('../pets/pets.router')
 const catsRouter = require('../cats/cats.router')
 const dogsRouter = require('../dogs/dogs.router')
+const errorHandler = require('../errorHandler')
 
 const app = express()
 
@@ -14,16 +15,4 @@ app.use('/api', peopleRouter)
 app.use('/api', petsRouter);
 app.use('/api', catsRouter);
 app.use('/api', dogsRouter);
-
-app.use(function errorHandler(error, req, res, next){
-    let response
-    console.error(error)
-    if(NODE_ENV === 'production'){
-        response = {error: { message: 'server error'}}
-    } else {
-        response = { message: error.message, error}
-    }
-    res.status(500).json(response)
-})
-
-module.exports = app
+app.use(errorHandler)
