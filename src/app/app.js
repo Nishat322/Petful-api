8346499f9753aa9c8f86185f17346ac2052da92c
Nishat Hoque <hoquen0397@gmail.com>
2021-04-01 20:18:14 -0400

    cors intall

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const { CLIENT_ORIGIN } = require('../../config')
 const peopleRouter = require('../people/people.router')
 const petsRouter = require('../pets/pets.router')
 const errorHandler = require('../errorHandler')
 
 const app = express()
 
-app.use(cors({origin: CLIENT_ORIGIN}))
+app.use(cors())
 
 app.use('/api', peopleRouter)
 app.use('/api', petsRouter);
